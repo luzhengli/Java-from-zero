28786d0bb4d6416c0d31a42d1d7750a9aa39ffa0
luzhengli <luzheng.li@outlook.com>
2020-10-25 20:20:14 +0800

    添加：13

---
+++ 13.泛型/Instance/src/App.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.out;
+
+import packg1.StringArrayList;
+import packg2.Person;
+import packg3.Pair;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        StringArrayList strArr = new StringArrayList();
+        System.out.println(strArr.getClass());
+
+        // 使用泛型：以 ArrayList 为例
+        ArrayList<String> strArrList = new ArrayList<String>(); // 泛型提供模板
+        strArrList.add("abc");
+        String s = strArrList.get(0); // 不用强制类型转换 编译器保证类型正确
+        out.println(s);
+        out.println(strArrList.getClass());
+
+        // ArrayList<T> 可以向上转型为 List<T>
+        List<String> list = new ArrayList<>(); // 可以省略<>中的list 编译器可以自动推断泛型类型
+
+        // 注意：不能把 ArrayList<Integer> 向上转型为 ArrayList<Number> 或 List<Number>
+        // ArrayList<Integer> integerList = new ArrayList<Integer>(); // 创建 ArrayList 对象
+        // integerList.add(new Integer(123)); // 添加 Integer 元素
+        // ArrayList<Number> numberList = integerList; // ArrayList<Integer> 转
+        // ArrayList<Number>
+        // numberList.add(new Float(12.34)); // 添加 Float 元素
+        // Integer n = numberList.get(1); // 获取到 Float 元素赋给引用变量 Integer 显然出错！！
+        // 可见 ArrayList<T> 中的元素不运行类型不同
+
+        // 泛型接口
+        Person[] personList = new Person[] { new Person("Ken", 30), new Person("Alice", 23), new Person("Bob", 34) };
+        Arrays.sort(personList);
+        out.println(Arrays.toString(personList));
+
+        // 编写泛型
+        Pair<String, Integer> pair = new Pair<String, Integer>("first", 20);
+        out.println(pair.getFirst()); // 实例方法在调用类后（会自动调用构造函数）就知道泛型类型了
+        out.println(pair.getLast()); 
+        Pair<Integer, String> pair2 = Pair.create(30, "last"); // 静态方法预先不知道泛型类型的具体类型
+        out.println(pair2.getFirst());
+        out.println(pair2.getLast());
+    }
+}
