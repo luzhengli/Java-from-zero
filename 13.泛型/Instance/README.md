eb5fd161cb54db1a8e4b301d5ebfd3c081ee496d
luzhengli <luzheng.li@outlook.com>
2020-10-26 12:27:13 +0800

    更新：13

--- 13.泛型/Instance/README.md
+++ 13.泛型/Instance/README.md
@@ -15,6 +15,127 @@
 
 详见：`App.java` 和 `Pair.java`
 
+# Java 的泛型是通过擦拭法实现
+
+## 擦拭法
+
+**Java 的泛型是由编译器实现的**，而虚拟机并不知道什么是泛型。具体来说编译器做了两件事情：
+
+1. 将 `<T>` 视为 `Object`
+2. 根据 `<T>` 的类型自动的实现安全的强制转型
+
+这种 Java 实现泛型的方式被称作**擦拭法**。
+
+
+
+**例子**：
+
+我们编写了一个泛型：
+
+```java
+public class Pair<T> {
+    private T first;
+    private T last;
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+}
+```
+
+经过编译器处理，虚拟机需要执行的代码变成了：
+
+```java
+public class Pair {
+    private Object first;
+    private Object last;
+    public Pair(Object first, Object last) {
+        this.first = first;
+        this.last = last;
+    }
+    public Object getFirst() {
+        return first;
+    }
+    public Object getLast() {
+        return last;
+    }
+}
+```
+
+使用泛型的时候，我们编写的代码也是编译器看到的代码：
+
+```java
+Pair<String> p = new Pair<>("Hello", "world");
+String first = p.getFirst();
+String last = p.getLast();
+```
+
+而虚拟机执行的代码并没有泛型：
+
+```java
+Pair p = new Pair("Hello", "world");
+String first = (String) p.getFirst(); // 编译器进行了自动安全的强制转型
+String last = (String) p.getLast();
+```
+
+
+
+## Java 泛型的局限
+
+了解到泛型是由擦拭法实现的，那么可以知道 Java 泛型的局限：
+
+1. **`<T>`不能是基本类型**，例如`int`。由于实际的类型是 `Object`，而 `Object` 无法持有基本类型。
+
+2. 不能取得泛型类的 `class`，不同泛型类型类的 `class` 都是一样的。即不存在 `Pair<T>.class`，只有唯一的 `Pair.class`。下面是一个例子
+
+    ```java
+    import static java.lang.System.out;
+    import packg3.Pair; // 详见 `packg3.Pair.java`
+    
+    public class App {
+        public static void main(String[] args) throws Exception {
+            // 擦拭法
+            Pair<String, Integer> p1 = new Pair<String, Integer>("first", 20);
+            Pair<Float,String> p2 = new Pair<Float,String>(3.21f, "last");
+            Class c1 = p1.getClass();
+            Class c2 = p2.getClass();
+            
+            out.println(c1);
+            out.println(c1);
+            out.println(c1 == c2);
+            out.println(c1 == Pair.class);
+            out.println(c2 == Pair.class);
+        }
+    }
+    ```
+
+    输出：
+
+    ```
+    class packg3.Pair
+    class packg3.Pair
+    true
+    true
+    true
+    ```
+
+3. 无法判断带泛型的类型，理由同 2。例子，以下代码是不合法的
+
+    ```java
+    Pair<String, String> p = new Pair<>("123", "456");
+    // Compile error:
+    if (p instanceof Pair<String, String>) {
+    }
+    ```
+
+4. 不能实例化 `<T>`
+
 # 总结
 
 - 从功能上来说，泛型是一个模板。它的优点是避免了强制类型转换，而类型的正确性交由编译器检查。
--- 13.泛型/Instance/src/App.java
+++ 13.泛型/Instance/src/App.java
@@ -10,19 +10,19 @@ import packg3.Pair;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        StringArrayList strArr = new StringArrayList();
-        System.out.println(strArr.getClass());
+        // System.out.println("Hello, World!");
+        // StringArrayList strArr = new StringArrayList();
+        // System.out.println(strArr.getClass());
 
         // 使用泛型：以 ArrayList 为例
-        ArrayList<String> strArrList = new ArrayList<String>(); // 泛型提供模板
-        strArrList.add("abc");
-        String s = strArrList.get(0); // 不用强制类型转换 编译器保证类型正确
-        out.println(s);
-        out.println(strArrList.getClass());
+        // ArrayList<String> strArrList = new ArrayList<String>(); // 泛型提供模板
+        // strArrList.add("abc");
+        // String s = strArrList.get(0); // 不用强制类型转换 编译器保证类型正确
+        // out.println(s);
+        // out.println(strArrList.getClass());
 
         // ArrayList<T> 可以向上转型为 List<T>
-        List<String> list = new ArrayList<>(); // 可以省略<>中的list 编译器可以自动推断泛型类型
+        // List<String> list = new ArrayList<>(); // 可以省略<>中的list 编译器可以自动推断泛型类型
 
         // 注意：不能把 ArrayList<Integer> 向上转型为 ArrayList<Number> 或 List<Number>
         // ArrayList<Integer> integerList = new ArrayList<Integer>(); // 创建 ArrayList 对象
@@ -34,16 +34,34 @@ public class App {
         // 可见 ArrayList<T> 中的元素不运行类型不同
 
         // 泛型接口
-        Person[] personList = new Person[] { new Person("Ken", 30), new Person("Alice", 23), new Person("Bob", 34) };
-        Arrays.sort(personList);
-        out.println(Arrays.toString(personList));
+        // Person[] personList = new Person[] { new Person("Ken", 30), new
+        // Person("Alice", 23), new Person("Bob", 34) };
+        // Arrays.sort(personList);
+        // out.println(Arrays.toString(personList));
 
         // 编写泛型
-        Pair<String, Integer> pair = new Pair<String, Integer>("first", 20);
-        out.println(pair.getFirst()); // 实例方法在调用类后（会自动调用构造函数）就知道泛型类型了
-        out.println(pair.getLast()); 
-        Pair<Integer, String> pair2 = Pair.create(30, "last"); // 静态方法预先不知道泛型类型的具体类型
-        out.println(pair2.getFirst());
-        out.println(pair2.getLast());
+        // Pair<String, Integer> pair = new Pair<String, Integer>("first", 20);
+        // out.println(pair.getFirst()); // 实例方法在调用类后（会自动调用构造函数）就知道泛型类型了
+        // out.println(pair.getLast());
+        // Pair<Integer, String> pair2 = Pair.create(30, "last"); // 静态方法预先不知道泛型类型的具体类型
+        // out.println(pair2.getFirst());
+        // out.println(pair2.getLast());
+
+        // 擦拭法
+        Pair<String, Integer> p1 = new Pair<String, Integer>("first", 20);
+        Pair<Float,String> p2 = new Pair<Float,String>(3.21f, "last");
+        Class c1 = p1.getClass();
+        Class c2 = p2.getClass();
+        out.println(c1);
+        out.println(c1);
+        out.println(c1 == c2);
+        out.println(c1 == Pair.class);
+        out.println(c2 == Pair.class);
+
+        Pair<String, String> p = new Pair<String, String>("123", "456");
+        // Compile error:
+        if (p instanceof Pair<?, ?>) {
+            
+        }
     }
 }
