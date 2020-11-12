4afa3074c03665b152f3f3743a1c6ed808b71b1e
luzhengli <luzheng.li@outlook.com>
2020-11-12 20:41:37 +0800

    添加笔记

--- Java-从0开始（基础）/21.多线程/thread-coordination(example)/thread-coordination(example)/src/App.java
+++ Java-从0开始（基础）/21.多线程/thread-coordination(example)/thread-coordination(example)/src/App.java
@@ -6,36 +6,37 @@ public class App {
     public static void main(String[] args) throws InterruptedException {
         var q = new TaskQueue();
         var ts = new ArrayList<Thread>();
+        // 启动5个 task出队线程
         for (int i=0; i<5; i++) {
-            // 执行5次获取task的线程
             var t = new Thread() {
                 public void run() {
-                    // 获取task:
+                    // 尝试获取task:
                     while (true) {
                         try {
                             String s = q.getTask();
                             System.out.println("execute task: " + s);
                         } catch (InterruptedException e) {
-                            return;
+                            return; // 被中断时就结束线程
                         }
                     }
                 }
             };
             t.start(); // 先启动线程
-            ts.add(t); // 再加到列表
+            ts.add(t); // 把启动了的线程添加到ArrayList
         }
+        // 启动1个task入队线程 （执行10次入队操作）
         var add = new Thread(() -> {
             for (int i=0; i<10; i++) {
-                // 放入task:
                 String s = "t-" + Math.random();
                 System.out.println("add task: " + s);
                 q.addTask(s);
-                try { Thread.sleep(100); } catch(InterruptedException e) {}
+                try { Thread.sleep(100); } catch(InterruptedException e) {} // 等待100ms
             }
         });
         add.start();
         add.join();
         Thread.sleep(100);
+        // 使用中断结束5个获取task入队线程 否则它们将一直陷入while循环状态
         for (var t : ts) {
             t.interrupt();
         }
